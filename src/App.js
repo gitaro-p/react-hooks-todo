ed9869d3e920377a3c6b789edbc4d2ed9bda46ec
清川　裕太郎 <yuu7353@gmail.com>
2020-07-14 00:48:17 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(() => {
+    console.log('useEffect is invoked.')
+  })
+
+  useEffect(() => {
+    console.log('useEffect is invoked.')
+  }, [])
+
+  useEffect(() => {
+    console.log('This vallback is for name only.')
+  }, [name])
+
+
   
   return (
     <>
